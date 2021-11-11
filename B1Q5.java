1283087640cfa72eced420eb78fe061de7038469
TheDucky <dakshkuldeep12@gmail.com>
2021-11-11 21:14:15 +0530

    removed unwanted folders

---
+++ B1Q5.java
@@ -0,0 +1,16 @@
+public class B1Q1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("name: daksh | AKA-TheDucky");
+    }
+}
+
+/*
+5. Write a Java program that takes two numbers as input and display the product of two numbers. Go to the editor
+Test Data:
+Input first number: 25
+Input second number: 5
+Expected Output :
+25 x 5 = 125
+*/

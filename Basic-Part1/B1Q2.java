7c17a309077e000bc7f0908f827533db2f623d19
TheDucky <dakshkuldeep12@gmail.com>
2021-11-11 21:10:20 +0530

    java code modified

--- Basic-Part1/B1Q2.java
+++ Basic-Part1/B1Q2.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
-
 public class B1Q2 {
 
     public static void main(String[] args) {
         System.out.println("~~ ADDING 2 NUMBERS ~~");
-        Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter First Number:");
-        int fn = scan.nextInt();
-        System.out.print("Enter Second Number:");
-        int sn = scan.nextInt();
-        scan.close();
-
-        int ans = fn + sn;
-        System.out.println(fn + " + " + sn + " = " + ans);
+        int ans = 74 + 36;
+        System.out.println(ans);
     }
 }
 
--- Basic-Part1/B1Q3.java
+++ Basic-Part1/B1Q3.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
-
 public class B1Q3 {
 
     public static void main(String[] args) {
         System.out.println("~~ DIVIDING 2 NUMBERS ~~");
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Dividend Number:");
-        int Dividend = sc.nextInt();
-        System.out.print("Enter Divisor Number:");
-        int Divisor = sc.nextInt();
-        sc.close();
-
-        int Quotient = Dividend / Divisor;
-        System.out.println(Dividend + " / " + Divisor + " = " + Quotient);
+        int Quotient = 50/3;
+        System.out.println(Quotient);
     }
 }
 

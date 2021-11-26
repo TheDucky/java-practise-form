888baf4ff6ce3c4c4c4c42c2dbfdc24ca2690184
TheDucky <dakshkuldeep12@gmail.com>
2021-11-26 19:39:02 +0530

    Exc8.java added

---
+++ CodeThingsHere/Exc8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exc8 {
+
+    public static void main(String[] args) {
+
+        //(°F − 32) × 5/9 = °C
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
+
+
+
+/*
+1. Write a Java program to convert temperature from Fahrenheit to Celsius degree. Go to the editor
+Test Data
+Input a degree in Fahrenheit: 212
+Expected Output:
+212.0 degree Fahrenheit is equal to 100.0 in Celsius
+*/

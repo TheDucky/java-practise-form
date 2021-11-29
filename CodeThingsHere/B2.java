0def5e9b1dee28dac0454428c8aedca0ac96fced
TheDucky <dakshkuldeep12@gmail.com>
2021-11-29 22:04:13 +0530

    B1 & B2 complete

--- CodeThingsHere/B2.java
+++ CodeThingsHere/B2.java
@@ -2,5 +2,15 @@ public class B2 {
 
     public static void main(String[] args) {
         
+        double EZ = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println("Result = " + EZ);
     }
 }
+
+/*
+9. Write a Java program to compute the specified expressions and print the output. Go to the editor
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output
+2.138888888888889
+*/

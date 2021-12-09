23f8f37c079b11a5fad1bc7fa224c5d67388d52a
TheDucky <dakshkuldeep12@gmail.com>
2021-12-09 19:54:07 +0530

    DONE WITH B5 & B4

--- CodeThingsHere/B4.java
+++ CodeThingsHere/B4.java
@@ -1,7 +1,21 @@
+import java.util.Scanner; 
+
 public class B4 {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter 3 numbers: ");
+
+        double n1 = scan.nextInt();
+        double n2 = scan.nextInt();
+        double n3 = scan.nextInt();
+
+        double avg = (n1 + n2 + n3) / 3;
+
+        System.out.println("average of the 3 numbers is: " + avg);
 
     }
 }
+
+// 12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
--- CodeThingsHere/B5.java
+++ CodeThingsHere/B5.java
@@ -1,7 +1,32 @@
+import java.util.Scanner; 
+
 public class B5 {
 
     public static void main(String[] args) {
 
-        
+        Scanner BBQ = new Scanner(System.in);
+
+        System.out.print("Enter Width: ");
+        double wi = BBQ.nextDouble();
+
+        System.out.print("Enter Height: ");
+        double hi = BBQ.nextDouble();
+
+        double area = wi * hi;
+        double peri = (wi + hi) * 2;
+
+        System.out.println("Area is " + wi + " * " + hi + " = " + area);
+        System.out.println("Perimeter is 2 * (" + wi + " + " + hi + ") = " + peri);
     }
 }
+
+/*
+13. Write a Java program to print the area and perimeter of a rectangle. 
+Test Data:
+Width = 5.5 Height = 8.5
+
+Expected Output
+Area is 5.6 * 8.5 = 47.60
+Perimeter is 2 * (5.6 + 8.5) = 28.20
+*/
+

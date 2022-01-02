824a72a34ca7b2f9ddb709320acd294f4b8ef350
TheDucky <dakshkuldeep12@gmail.com>
2022-01-02 18:30:04 +0530

    B6 complete

--- CodeThingsHere/B6.java
+++ CodeThingsHere/B6.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class B6 {
 
     public static void main(String[] args) {
 
-        
+        int a, b, swap;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter 2 numbers");
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println("before number swap \n" + "num1 = " + a + " | num2 = " + b);
+
+        swap = a;
+        a = b;
+        b = swap;
+        System.out.println("after number swap \n" + "num1 = " + a + " | num2 = " + b);
+      
     }
 }
+
+// 15. Write a Java program to swap two variables. 
